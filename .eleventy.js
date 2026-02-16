516317efc3918208abd29dac2aef1825ee543a8d
adin-m <adin.mulagic@gmail.com>
2026-02-16 01:04:07 +0100

    11tyfied blog site

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,15 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
 
 eleventyConfig.setServerPassthroughCopyBehavior("copy");
 eleventyConfig.addPassthroughCopy('./src/css');
 eleventyConfig.addPassthroughCopy('./src/assets');
+
+eleventyConfig.addFilter("postDate", (dateObj) => {
+	return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+})
+
 
 return {
 	dir: {
